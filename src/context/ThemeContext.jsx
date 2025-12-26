054c2d2019b94c5cd69fbd799bdd60ff52e4e61e
herrykargar <herrykargar009@gmail.com>
2025-12-26 09:22:03 +0530

    remove extra code

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,30 +1,3 @@
-// export const ThemeContext = createContext();
-
-// export const useTheme = () => {
-//   const context = useContext(ThemeContext);
-//   if (!context) {
-//     throw new Error('useTheme must be used within a ThemeProvider');
-//   }
-//   return context;
-// };
-
-// export const ThemeProvider = ({ children }) => {
-//   const [theme, setTheme] = useState('light');
-//   console.log('Current Theme:', theme);
-
-//   const toggleTheme = () => {
-//     setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-//       <div className={theme === 'dark' ? 'dark' : 'light'}>
-//         {children}
-//       </div>
-//     </ThemeContext.Provider>
-//   );
-// };
-
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext();
